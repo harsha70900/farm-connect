c09b52af7ec17fdb56fb820c3358a53990b0b844
Harshavardhan S V <harshavardhan70900@gmail.com>
2026-05-22 20:09:09 +0530

    Implemented product persistence using localStorage and Context API

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,28 +1,47 @@
-import { createContext, useState } from 'react'
+import {
+  createContext,
+  useState,
+  useEffect,
+} from 'react'
 
 export const ProductContext = createContext()
 
 function ProductProvider({ children }) {
 
-  const [products, setProducts] = useState([
-    {
-      id: 1,
-      name: 'Fresh Tomatoes',
-      category: 'Vegetables',
-      price: 40,
-      image:
-        'https://images.unsplash.com/photo-1546094096-0df4bcaaa337?q=80&w=1200&auto=format&fit=crop',
-    },
+ const [products, setProducts] = useState(() => {
 
-    {
-      id: 2,
-      name: 'Premium Rice',
-      category: 'Grains',
-      price: 70,
-      image:
-        'https://images.unsplash.com/photo-1586201375761-83865001e31c?q=80&w=1200&auto=format&fit=crop',
-    },
-  ])
+  const savedProducts =
+    localStorage.getItem('products')
+
+  return savedProducts
+    ? JSON.parse(savedProducts)
+    : [
+        {
+          id: 1,
+          name: 'Fresh Tomatoes',
+          category: 'Vegetables',
+          price: 40,
+          image:
+            'https://images.unsplash.com/photo-1546094096-0df4bcaaa337?q=80&w=1200&auto=format&fit=crop',
+        },
+
+        {
+          id: 2,
+          name: 'Premium Rice',
+          category: 'Grains',
+          price: 70,
+          image:
+            'https://images.unsplash.com/photo-1586201375761-83865001e31c?q=80&w=1200&auto=format&fit=crop',
+        },
+      ]
+})
+
+useEffect(() => {
+  localStorage.setItem(
+    'products',
+    JSON.stringify(products)
+  )
+}, [products])
 
   return (
     <ProductContext.Provider
